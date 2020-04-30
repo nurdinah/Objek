1359526b56a705c219d25d2b4d8bd2b00e57cf92
nurdinah <dinah.saja.bjb@gmail.com>
2020-04-30 16:01:15 +0700

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
 public class Main {
+    public static void main(String[] args) {
+        //instance
+        Worm player1 = new Worm();
+        Worm player2 = new Worm();
+
+        player1.username = "Nurdinah";
+        player1.score = 500;
+        player1.length = 3.5;
+
+        player2.username = "Iqbal";
+        player2.score = 200;
+        player2.length = 1.5;
+
+        player1.eatSnank();
+
+        System.out.println(player1.score);
+        player1.eatOthers(player2);
+        System.out.println(player1.score);
+
+
+    }
 }
---
+++ src/Round.java
@@ -0,0 +1,10 @@
+public class Round {
+
+    int roundTime;
+    Worm winner;
+    Worm player1;
+    Worm player2;
+    Worm player3;
+    Worm player4;
+
+}
